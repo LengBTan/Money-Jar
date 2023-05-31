a9f2b3f2d999aed931b24039fdbf0460823aeedf
WinterRadish <55962828+WinterRadish@users.noreply.github.com>
2023-05-30 21:33:00 -0400

    Add variables for piezo and photoresistor

--- Tip_Jar/Tip_Jar.ino
+++ Tip_Jar/Tip_Jar.ino
@@ -3,6 +3,9 @@
 //LCD pin variables
 const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const int piezo = 9; // pin variable for piezo
+const int sensorPin = 0;// pin variable for photoresitor
 void setup(){
 
 }
