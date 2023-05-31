c8ac81f77e905def6d2c37377b52fdc4fb0e39df
WinterRadish <55962828+WinterRadish@users.noreply.github.com>
2023-05-30 21:49:19 -0400

    Update setup() function

--- Tip_Jar/Tip_Jar.ino
+++ Tip_Jar/Tip_Jar.ino
@@ -9,6 +9,11 @@ const int sensorPin = 0;// pin variable for photoresitor
 void setup(){
 const int LCDBacklight = 8;// pin for the LCD backlight
 
+void setup(){
+  Serial.begin(9600);
+  pinMode(piezo, OUTPUT);//set piezo pin as a output
+  lcd.begin(16, 2);//initialize LCD, 16 columns and 2 rows
+  digitalWrite(LCDBacklight, LOW);//set LCD backlight to an off state
 }
 
 void loop(){
